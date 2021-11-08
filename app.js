30f66e92452303e242683cfa33c3a1618a76ca5e
Jyll Wright <jyllanawright@gmail.com>
2021-11-08 14:05:08 -0500

    Changed some insertAdjacent code to work as innerHTML

--- app.js
+++ app.js
@@ -12,19 +12,24 @@ async function loadPokemon() {
             try {
                 newPokemon = await axios.get(`${poke.url}`);
                 newPokemon = newPokemon.data;
-    
-                div.insertAdjacentHTML(`beforeend`, `<div id="${newPokemon.name}" class="pokemon"></div>`);
-                let newDiv = document.querySelector(`#${newPokemon.name}`);
-                newDiv.insertAdjacentHTML(`beforeend`, `<div class="img-container"><img src="${newPokemon.sprites.front_default}" alt = "${newPokemon.name}"></div>`);
-                newDiv.insertAdjacentHTML(`beforeend`, `<h2>${newPokemon.name}</h2>`);
+
+                let newDiv = document.createElement(`div`);
+                newDiv.classList.add(`pokemon`)
+                div.append(newDiv);
+
+                newDiv.innerHTML = `<div class="img-container">
+                <img src="${newPokemon.sprites.front_default}" alt = "${newPokemon.name}">
+                </div>
+                <h2>${newPokemon.name}</h2>`;
+            
             } catch (err) {
-                console.log(err);
+                console.log(`Failed to Get Pokemon Info: ${err}`);
             }
             
         }
         console.log(pokemon);
     } catch (err) {
-        console.log(err);
+        console.log(`Failed to Get Pokemon List: ${err}`);
     }
 
 }
